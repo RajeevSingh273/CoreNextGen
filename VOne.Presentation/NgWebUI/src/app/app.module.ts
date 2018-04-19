873b4c7bfa277942562ed4c2e6b1e27f299b51f1
DESKTOP-3OQGOBF\rajee <rajeevsingh273@gmail.com>
2018-04-19 21:51:17 +0530

    Code Merge

--- VOne.Presentation/NgWebUI/src/app/app.module.ts
+++ VOne.Presentation/NgWebUI/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app.routing.module";
+import { AppComponent } from "./app.component";
+import { LandingComponent } from "./modules/client/landing/landing.component";
+import { NavigationComponent } from "./modules/client/shared/navigation/navigation.component";
+import { FooterComponent } from "./modules/client/shared/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavigationComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
