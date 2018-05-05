96d256cb5167b8f0473b384616f38653e58c14d5
RajeevSingh273 <rajeevsingh273@gmail.com>
2018-05-05 23:25:23 +0530

    Upgraded to Angular 6

--- VOne.Presentation/NgWebUI/src/app/pagelayout/layout-client/layout-client.component.ts
+++ VOne.Presentation/NgWebUI/src/app/pagelayout/layout-client/layout-client.component.ts
@@ -1,16 +1,21 @@
-import { NavigationComponent } from './../../modules/client/components/shared/navigation/navigation.component';
-import { Component, OnInit, ViewChild, Renderer, Inject, ElementRef } from '@angular/core';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
-import { Subscription } from 'rxjs/Subscription';
-import { DOCUMENT } from '@angular/platform-browser';
-import { Router, NavigationEnd } from '@angular/router';
-import 'rxjs/add/operator/filter';
-
+import { NavigationComponent } from "./../../modules/client/components/shared/navigation/navigation.component";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Renderer,
+  Inject,
+  ElementRef
+} from "@angular/core";
+import { LocationStrategy, PlatformLocation, Location } from "@angular/common";
+import { Subscription } from "rxjs";
+import { DOCUMENT } from "@angular/platform-browser";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-layout-client',
-  templateUrl: './layout-client.component.html',
-  styleUrls: ['./layout-client.component.css']
+  selector: "app-layout-client",
+  templateUrl: "./layout-client.component.html",
+  styleUrls: ["./layout-client.component.css"]
 })
 export class LayoutClientComponent implements OnInit {
   private _router: Subscription;
@@ -24,13 +29,12 @@ export class LayoutClientComponent implements OnInit {
     @Inject(DOCUMENT) private document: any,
     private element: ElementRef,
     public location: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this._title = this.location.prepareExternalUrl(this.location.path());
     this._navbar = this.element.nativeElement.children[0].children[0];
     this._router = this.router.events
-      .filter(event => event instanceof NavigationEnd)
       .subscribe((event: NavigationEnd) => {
         this._title = this.location.prepareExternalUrl(this.location.path());
         if (window.outerWidth > 991) {
@@ -39,48 +43,44 @@ export class LayoutClientComponent implements OnInit {
           window.document.activeElement.scrollTop = 0;
         }
         this.navbar.sidebarClose();
-        if (this._title === '/' || this._title === '/home') {
-          this._navbar.classList.add('navbar-transparent');
+        if (this._title === "/" || this._title === "/home") {
+          this._navbar.classList.add("navbar-transparent");
         } else {
-          this._navbar.classList.remove('navbar-transparent');
+          this._navbar.classList.remove("navbar-transparent");
         }
-
       });
 
-    this.renderer.listenGlobal('window', 'scroll', event => {
-      if (this._title === '/' || this._title === '/home') {
+    this.renderer.listenGlobal("window", "scroll", event => {
+      if (this._title === "/" || this._title === "/home") {
         const number = window.scrollY;
         if (number > 150 || window.pageYOffset > 150) {
-          this._navbar.classList.remove('navbar-transparent');
+          this._navbar.classList.remove("navbar-transparent");
         } else {
-          this._navbar.classList.add('navbar-transparent');
+          this._navbar.classList.add("navbar-transparent");
         }
       } else {
-        this._navbar.classList.remove('navbar-transparent');
+        this._navbar.classList.remove("navbar-transparent");
       }
     });
     const ua = window.navigator.userAgent;
-    const trident = ua.indexOf('Trident/');
+    const trident = ua.indexOf("Trident/");
     if (trident > 0) {
       // IE 11 => return version number
-      const rv = ua.indexOf('rv:');
-      const version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+      const rv = ua.indexOf("rv:");
+      const version = parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
       if (version) {
-        const body = document.getElementsByTagName('body')[0];
-        body.classList.add('ie-background');
+        const body = document.getElementsByTagName("body")[0];
+        body.classList.add("ie-background");
       }
     }
   }
 
-
-
   removeFooter() {
     this._title = this._title.slice(1);
-    if (this._title === 'signup' || this._title === 'nucleoicons') {
+    if (this._title === "signup" || this._title === "nucleoicons") {
       return false;
     } else {
       return true;
     }
   }
 }
-
