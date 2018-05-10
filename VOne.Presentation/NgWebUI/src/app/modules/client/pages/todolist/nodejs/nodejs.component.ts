eca2c6b64306b2ea6c6d7705d6420f525921611a
RajeevSingh273 <rajeevsingh273@gmail.com>
2018-05-10 17:58:06 +0530

    Update nodejs.component.ts

--- VOne.Presentation/NgWebUI/src/app/modules/client/pages/todolist/nodejs/nodejs.component.ts
+++ VOne.Presentation/NgWebUI/src/app/modules/client/pages/todolist/nodejs/nodejs.component.ts
@@ -2,7 +2,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AddEditListComponent } from './add-edit-list/add-edit-list.component';
 import { NgForm } from '@angular/forms';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { TodoService } from '../todo.service';
+import { Todo } from '../../../../../models/todo.model';
+
+
 
 @Component({
   selector: 'app-nodejs',
@@ -13,13 +17,20 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 export class NodejsComponent implements OnInit {
   // public toggelPopUp = false;
   @ViewChild('addMountForm') addMountForm: NgForm;
-  constructor(private modalService: NgbModal) { }
+  todoModel: Todo;
+  updatedList: Todo[] = [];
+  modalReference: NgbModalRef;
+  todoList: Todo = new Todo(0, '', '', 0, 0, new Date(), new Date());
+
+  constructor(private modalService: NgbModal,
+    private todoService: TodoService, ) { }
 
   ngOnInit() {
   }
 
   open(content) {
-    this.modalService.open(content).result.then((result) => {
+    this.modalReference = this.modalService.open(content);
+    this.modalReference.result.then((result) => {
       console.log('closed');
     }, (reason) => {
       console.log('dismissed');
@@ -29,5 +40,19 @@ export class NodejsComponent implements OnInit {
   onSubmit(form: NgForm) {
     console.log('adding form values ');
     console.log(form.value);
+    let list = this.todoService.getToDoList();
+
+    this.updatedList.push(new Todo(1, form.value.title, form.value.description, 1, 1, new Date(), new Date()));
+
+    // this.todoService.setToDoList(list);
+    // this.updatedList = this.todoService.getToDoList(); 
+    console.log(list);
+    this.modalReference.close();
 
   }
+  editContent(todo) {
+    console.log(todo);
+    this.open(todo);
+    this.todoList = new Todo(1, todo.title, todo.description, 1, 1, new Date(), new Date());
+  }
+}
